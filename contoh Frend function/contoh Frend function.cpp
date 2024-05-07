77faea1c56858f9f34a2623b2450a4e21c365d84
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-05-07 11:20:35 +0700

    Menambahkan int class mahaiswa 3

--- contoh Frend function/contoh Frend function.cpp
+++ contoh Frend function/contoh Frend function.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>;
 using namespace std;
 
 class mahasiswa
@@ -7,45 +8,19 @@ private:
     string nama;
 
 public:
-    friend void setnama(mahasiswa& a, string);
+    friend void setNama(mahasiswa& a, string);
 };
 
 void setNama(mahasiswa& a, string pNama)
 {
-    a, nama = pNama;
+    a.nama = pNama;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 int main()
 {
-    
+    mahasiswa joko;
+    setNama(joko, "Kairi Kumar");
+    return 0;
 }
 
