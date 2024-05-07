6d026a1bd3bb8e097b3ca98c31a52343ff1d392f
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-05-07 11:29:36 +0700

    Menambahkan input class pelajar

---
+++ contoh Friend pointer 2/contoh Friend pointer 2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class pelajar;
+class manusia
+{
+public:
+    void showNilaiPelajar(pelajar& a);
+};
+
+class pelajar
+{
+private:
+    int nilai;
+
+public:
+    pelajar() { nilai = 100; }
+    friend void manusia::showNilaiPelajar(pelajar& x);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+    
+}
+
