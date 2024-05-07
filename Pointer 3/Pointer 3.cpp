c2be1d205b05fddd1bd89172788c27f308c94772
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-05-07 11:06:51 +0700

    Menambahkan display void siswa & void orang

--- Pointer 3/Pointer 3.cpp
+++ Pointer 3/Pointer 3.cpp
@@ -22,6 +22,22 @@ public:
     void displayAll(orang& a);
 };
 
+void siswa::displayAll(orang& a)
+{
+    cout << id << endl << a.nama;
+}
+
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
+
+void siswa::setId(int pId)
+{
+    id = pId;
+}
+
+
 
 
 
